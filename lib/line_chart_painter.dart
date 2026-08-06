48d5a8f462c25af36440d79917a529e7b0949346
rtvoorhees <voorhees@measurenet-tech.com>
2026-08-06 12:35:18 -0400

    DIAGNOSTIC: replace chart with simple fixed-size box to isolate rendering issue

--- lib/line_chart_painter.dart
+++ lib/line_chart_painter.dart
@@ -1,4 +1,3 @@
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class LiveLineChart extends StatelessWidget {
@@ -15,58 +14,20 @@ class LiveLineChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final spots = <FlSpot>[
-      for (int i = 0; i < data.length; i++)
-        FlSpot(i.toDouble(), data[i].toDouble()),
-    ];
-
-    double minY = 0;
-    double maxY = 1;
-    if (data.isNotEmpty) {
-      minY = data.reduce((a, b) => a < b ? a : b).toDouble();
-      maxY = data.reduce((a, b) => a > b ? a : b).toDouble();
-      if (minY == maxY) {
-        minY -= 1;
-        maxY += 1;
-      }
-    }
-
-    final maxX = data.isEmpty ? 1.0 : (data.length - 1).toDouble();
-
+    // TEMPORARY DIAGNOSTIC: plain fixed-size box, no chart library involved.
     return SizedBox(
       height: height,
       width: double.infinity,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey.shade300),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(4),
-            child: spots.length < 2
-                ? const SizedBox.shrink()
-                : LineChart(
-                    LineChartData(
-                      minX: 0,
-                      maxX: maxX,
-                      minY: minY,
-                      maxY: maxY,
-                      gridData: const FlGridData(show: false),
-                      titlesData: const FlTitlesData(show: false),
-                      borderData: FlBorderData(show: false),
-                      lineTouchData: const LineTouchData(enabled: false),
-                      lineBarsData: [
-                        LineChartBarData(
-                          spots: spots,
-                          isCurved: false,
-                          color: color,
-                          barWidth: 2,
-                          dotData: const FlDotData(show: false),
-                        ),
-                      ],
-                    ),
-                  ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: color.withOpacity(0.15),
+          border: Border.all(color: Colors.grey.shade300),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            'points: ${data.length}',
+            style: TextStyle(color: color),
           ),
         ),
       ),
